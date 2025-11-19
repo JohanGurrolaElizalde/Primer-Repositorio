cb327a6a68fd10a057c6d776c4d0b362ed121f19
JohanGurrolaElizalde <johan.gurrola.elizalde@cbtis89.edu.mx>
2025-11-19 09:20:09 -0600

    Delete Pr01_Hola.py

--- Pr01_Hola.py
+++
@@ -1,6 +0,0 @@
-#Este es mi primer programa
-#Autor: Gurrola Elizalde Johan
-#Objetivo: Saludar al usuario y dar informacion
-print ("Hola Mundo")
-print ("Mi Nombre es Johan")
-print ("Me Gusta La Hamburguesa")

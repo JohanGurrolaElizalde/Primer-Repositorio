9c01e984dbe6145892ec5504b08b761dfe02dae7
JohanGurrolaElizalde <johan.gurrola.elizalde@cbtis89.edu.mx>
2025-11-19 09:06:55 -0600

    practica 1
    Esta practica muestra el mensaje "Hola Mundo" y mis gustos

---
+++ Pr01_Hola.py
@@ -0,0 +1,6 @@
+#Este es mi primer programa
+#Autor: Gurrola Elizalde Johan
+#Objetivo: Saludar al usuario y dar informacion
+print ("Hola Mundo")
+print ("Mi Nombre es Johan")
+print ("Me Gusta La Hamburguesa")
